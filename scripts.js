68e56e76fb7806fcadbd43ea4fe32c97ea1b2314
xjh2009 <102580788+xjh2009@users.noreply.github.com>
2024-07-16 23:23:25 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -16,8 +16,8 @@ async function runTest(fileName) {
                         jsonList.appendChild(listItem);
                     });
                     const endTime = performance.now();
-                    const duration = (endTime - startTime) / 1000; // 转换为秒
-                    resultText.textContent = `解析 ${fileName} 条Json文件 用时 ${duration.toFixed(2)} 秒。`;
+                    const duration = (endTime - startTime)
+                    resultText.textContent = `解析 ${fileName} 条Json文件 用时 ${duration.toFixed(2)} 毫秒。`;
 
             } catch (error) {
                 jsonList.textContent = '加载或解析 JSON 数据时出错。';
@@ -41,8 +41,8 @@ async function runCreate(cs) {
                     jsonList.innerHTML = JSON.parse(JSON.stringify(jsonData)); 
 
                     const endTime = performance.now();
-                    const duration = (endTime - startTime) / 1000; // 转换为秒
-                    resultText.textContent = `生成 ${cs} 条Json数据 用时 ${duration.toFixed(2)} 秒。`;
+                    const duration = (endTime - startTime); 
+                    resultText.textContent = `生成 ${cs} 条Json数据 用时 ${duration.toFixed(2)} 毫秒。`;
 
             } catch (error) {
                 resultText.textContent = '加载或解析 JSON 数据时出错。';
